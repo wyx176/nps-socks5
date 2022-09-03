5a73e3c2e314942eb265357bd4b3e1292733b190
dev <32320103+wyx176@users.noreply.github.com>
2022-09-03 21:42:05 +0800

    更新安装程序
    增加安装客户端、以及卸载等

--- install.sh
+++ install.sh
@@ -11,16 +11,37 @@ clientUrl=${downLoadUrl}${version}/${clientSoft}.tar.gz
 s5Path=/opt/nps-socks5/
 ipAdd=检测失败
 
-clearData(){
-	cd ${s5Path}${clientSoft} && nps uninstall
-	cd ${s5Path}${clientSoft} && ./npc uninstall
-   
-	#删除之前的
+init(){
+yum install git unzip wget -y
+yum -y install curl
+}
+
+unstallServer(){
+	
+	cd ${s5Path}${serverSoft} && nps stop && nps uninstall
+	rm -rf /etc/nps
+	rm -rf /usr/bin/nps
+	rm -rf ${s5Path}${serverSoft}
+	echo "卸载服务端成功"
+	
+}
+
+unstallClient(){
+	cd ${s5Path}${clientSoft} && npc stop &&  ./npc uninstall
+	rm -rf /usr/bin/nps
+	rm -rf ${s5Path}${clientSoft}
+	echo "卸载客户端成功"
+}
+
+allUninstall(){
+unstallServer
+unstallClient
+#删除之前的
 	rm -rf ${s5Path}
 }
 
 checkIp(){
-yum -y install curl
+
 ipAdd=`curl http://ifconfig.info -s --connect-timeout 10`
 clear
 echo "当前ip地址："${ipAdd}
@@ -36,15 +57,11 @@ read -p "如果不对请停止安装或者手动输入服务器ip：(y/n/ip)： 
 	elif [[ "$choice" != 'y' && "$choice" != 'Y' && "${choice}" != '' ]]; then
 		check_ip "${choice}"
 	fi
-	
-	clearData
 }
 
-#2.下载Socks5服务
-Download()
+#2.下载服务端
+DownloadServer()
 {
-yum install git unzip wget -y
-echo ""
 echo "下载nps-socks5服务中请耐心等待..."
 if [[ ! -d ${s5Path} ]];then
 	mkdir -p ${s5Path}	
@@ -52,20 +69,32 @@ fi
 
 #服务端
 wget -P ${s5Path} --no-cookie --no-check-certificate ${serverUrl} 2>&1 | progressfilt
-#客户端
-wget -P ${s5Path} --no-cookie --no-check-certificate ${clientUrl} 2>&1 | progressfilt
+
 
 if [[ ! -f ${s5Path}${serverSoft}.tar.gz ]]; then
 	echo "服务端文件下载失败"${errorMsg}
 	exit 0
 fi
 
+}
+
+DownloadClient()
+{
+echo "下载nps-socks5客户端中请耐心等待..."
+if [[ ! -d ${s5Path} ]];then
+	mkdir -p ${s5Path}	
+fi
+
+
+#客户端
+wget -P ${s5Path} --no-cookie --no-check-certificate ${clientUrl} 2>&1 | progressfilt
+
+
 if [[ ! -f ${s5Path}${clientSoft}.tar.gz ]]; then
 	echo "客户端文件下载失败"${errorMsg}
 	exit 0
 fi
 }
-
 
 #3.安装Socks5服务端程序
 InstallServer()
@@ -90,36 +119,45 @@ mkdir -p ${s5Path}${clientSoft}
 fi
 tar zxvf ${s5Path}${clientSoft}.tar.gz -C ${s5Path}${clientSoft}
 
+clear
 echo "客户端文件安装中..."
 cd ${s5Path}${clientSoft}
+if [[ $menuChoice == 1 ]];then
 ./npc install  -server=${ipAdd}:8025 -vkey=ij7poeu2d9btjbd3 -type=tcp && npc start
+else
+echo "服务器参数在[服务端]->服务列表+号中"
+echo "类似：./npc -server=xxx.xxx.xxx.172:8025 -vkey=ij7poeu2d9btjbd3 -type=tcp"
+echo "只需要输入:-server=xxx.xxx.xxx.172:8025 -vkey=ij7poeu2d9btjbd3 -type=tcp 即可"
+read -p "请输入服务端参数： " serverParam
+./npc install ${serverParam} && npc start
+fi
 }
 
-checkInstall(){
+
+
+checkServer(){
 #检查服务端是否安装成功
 SPID=`ps -ef|grep nps |grep -v grep|awk '{print $2}'`
 if [[ -z ${SPID} ]]; then
 echo ${SPID}"SPID----------------------"
 echo "服务端安装失败"${errorMsg}
-clearData
+unstallServer
 exit 0
 fi
+}
+
+
+checkClient(){
 
 CPID=`ps -ef|grep npc |grep -v grep|awk '{print $2}'`
 if [[ -z ${CPID} ]]; then
 echo "客户端安装失败"${errorMsg}
+unstallClient
 exit 0
-clearData
 fi
-
-clear
-echo "--安装成功------"${errorMsg}
-echo "--后台管理地址"${ipAdd}":"${webPort}
-echo "--登录账号admin"
-echo "--登录密码admin"
-echo "默认socks5账号信息:账号socks5 密码socks5 端口5555"
-echo "如需修改后台管理端口以及账号密码请看github"
 }
+
+
 
 function check_ip(){
         IP=$1
@@ -161,8 +199,84 @@ progressfilt ()
     done
 }
 
-checkIp
-Download
-InstallServer
-InstallClient
-checkInstall
+
+menu(){
+echo "1.全部安装"
+echo "2.安装服务端"
+echo "3.安装客户端"
+echo "4.卸载服务端"
+echo "5.卸载客户端"
+echo "6.全卸载"
+echo "0.退出"
+while :; do echo
+	read -p "请选择： " menuChoice
+	if [[ ! $menuChoice =~ ^[0-6]$ ]]; then
+		echo "输入错误! 请输入正确的数字!"
+	else
+		break	
+	fi
+done
+
+
+if [[ $menuChoice == 0 ]];then
+	exit 0
+fi	
+
+if [[ $menuChoice == 1 ]];then
+	#安装服务端
+	init
+	checkIp
+	
+	allUninstall
+	DownloadServer
+	DownloadClient
+	InstallServer
+	InstallClient
+	checkServer
+	checkClient
+	clear
+	echo "--安装成功------"${errorMsg}
+	echo "--后台管理地址"${ipAdd}":"${webPort}
+	echo "--登录账号admin"
+	echo "--登录密码admin"
+	echo "默认socks5账号信息:账号socks5 密码socks5 端口5555"
+	echo "如需修改后台管理端口以及账号密码请看github"
+
+fi
+if [[ $menuChoice == 2 ]];then
+	init
+	checkIp
+	unstallServer
+	DownloadServer
+	InstallServer
+	checkServer
+	clear
+	echo "--安装成功------"${errorMsg}
+	echo "--后台管理地址"${ipAdd}":"${webPort}
+	echo "--登录账号admin"
+	echo "--登录密码admin"
+fi
+
+if [[ $menuChoice == 3 ]];then
+	clear
+	unstallClient
+	DownloadClient
+	clear
+	InstallClient
+	checkClient
+	echo "--安装成功------"${errorMsg}
+fi
+if [[ $menuChoice == 4 ]];then
+unstallServer
+fi
+
+if [[ $menuChoice == 5 ]];then
+unstallClient
+fi
+
+if [[ $menuChoice == 6 ]];then
+allUninstall
+fi
+}
+menu
+
