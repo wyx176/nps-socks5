79441e3a8097f5723ce9ea0be7195881fea0fce7
dev <32320103+wyx176@users.noreply.github.com>
2022-10-04 14:51:21 +0800

    Add files via upload

---
+++ lib/daemon/reload.go
@@ -0,0 +1,25 @@
+//go:build !windows
+// +build !windows
+
+package daemon
+
+import (
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"ehang.io/nps/lib/common"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGUSR1)
+	go func() {
+		for {
+			<-s
+			beego.LoadAppConfig("ini", filepath.Join(common.GetRunPath(), "conf", "nps.conf"))
+		}
+	}()
+}
